7e3e9a81f94ec96603cb5f0b8f3552240ce56a66
Caio Matheus <kirodnv@gmail.com>
2025-04-10 11:01:04 -0300

    Atualizando página de cadastro de produtos

--- Pages/Cadastro.cshtml.cs
+++ Pages/Cadastro.cshtml.cs
@@ -8,27 +8,49 @@ public class CadastroModel : PageModel
 {
     [BindProperty]
 
-    public Produto Produto {get; set;} = new Produto(); // objeto que vai receber os dados do form
-    public string Mensagem { get;set;}
+    public Produto Produto { get; set; } = new Produto(); // objeto que vai receber os dados do form
+    public string Mensagem { get; set; }
     public void OnPost()
     {
-            using (var db = new AppDbContext()) {
-                var produtoExistente = db.Produtos.FirstOrDefault(p => p.Nome == Produto.Nome);
+        Produto.Nome = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(Produto.Nome.ToLower());
 
-                if (produtoExistente != null)
-                {
-                    produtoExistente.Quantidade += Produto.Quantidade;
+        // amanha tentar colocar o campo de "nome do produto" como uma lista de produtos já cadastrados no sistema, e o usuario vai ter a opçao de adicionar um novo produto na lista, 
+        // assim posso evitar o caso de produtos duplicados no banco
+        using (var db = new AppDbContext())
+        {
+            var produtoExistente = db.Produtos.FirstOrDefault(p => p.Nome == Produto.Nome);
 
-                    Mensagem = $"Produto {Produto.Nome} já existente. Quantidade atualizada";
-                } else {
-                    db.Produtos.Add(Produto);
+            if (produtoExistente != null)
+            {
+                produtoExistente.Quantidade += Produto.Quantidade;
 
-                    Mensagem = $"Produto {Produto.Nome} cadastrado com sucesso";
-                    
-                    System.Console.WriteLine("Produto cadastrado!");
-                }
-                
+                Mensagem = $"Produto {Produto.Nome} já existente. Quantidade atualizada";
+            }
+            else
+            {
+                db.Produtos.Add(Produto);
+
+                Mensagem = $"Produto {Produto.Nome} cadastrado com sucesso";
+
+                System.Console.WriteLine("Produto cadastrado!");
+            }
+
             db.SaveChanges();
         }
     }
+
+    public List<string> ProdutosCadastrados { get; set; } = new List<string>();
+
+    public void OnGet()
+    {
+        using (var db = new AppDbContext())
+        {
+            ProdutosCadastrados = db.Produtos
+                .Select(p => p.Nome)
+                .Distinct()
+                .OrderBy(n => n)
+                .ToList();
+        }
+    }
 }
+
--- obj/Debug/net9.0/Mercadinho.AssemblyInfo.cs
+++ obj/Debug/net9.0/Mercadinho.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mercadinho")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99e948af3ec784d8e67c6f1bef9435d0c191202b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f4b84155988b271d6990518752d1bc23d87661")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mercadinho")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mercadinho")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
