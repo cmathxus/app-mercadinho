bf901698cf7110da46a3ea0d3cc063ab56ae21d9
Caio Matheus <kirodnv@gmail.com>
2025-04-16 17:42:37 -0300

    Adicionado página para editar os produtos cadastrados

---
+++ Pages/EditarLista.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mercadinho.models;
+using System.Dynamic;
+
+public class EditarListaModel : PageModel
+    {
+        [BindProperty]
+        public Produto Produto {get;set;}
+
+        public async Task<IActionResult> OnGetAsync(int id) {
+            using (var db = new AppDbContext()) {
+                Produto = await db.Produtos.FindAsync(id);
+                if (Produto == null) {
+                    return RedirectToPage("/Lista");
+                }
+            }
+
+            return Page();
+        }
+
+
+        
+        public async Task<IActionResult> OnPostAsync () {
+            using (var db = new AppDbContext()) {
+                var produtoNoBanco = await db.Produtos.FindAsync(Produto.Id);
+
+                if (produtoNoBanco != null) {
+                    produtoNoBanco.Nome = Produto.Nome;
+                    produtoNoBanco.Preco = Produto.Preco;
+                    produtoNoBanco.Quantidade = Produto.Quantidade;
+
+                    await db.SaveChangesAsync();
+                }
+            }
+            return RedirectToPage("/Lista");
+        }
+    }
